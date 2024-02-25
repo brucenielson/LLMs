25b9f44dd21ebdc70ba116e764acad55a7f3d323
Bruce Nielson <bnielson@mindfiretechnology.com>
2024-02-24 17:43:23 -0700

    Refactor to allow loading not part of initialization

--- ebook_semantic_search.py
+++ ebook_semantic_search.py
@@ -9,55 +9,66 @@ import math
 import os
 
 
-class EBookSearch:
+class SemanticSearch:
 
     # noinspection SpellCheckingInspection
-    def __init__(self, full_file_name, model_name='sentence-transformers/multi-qa-mpnet-base-dot-v1', do_preview=False,
-                 do_strip=True, first_chapter=0, last_chapter=math.inf):
+    def __init__(self, full_file_name=None, model_name='sentence-transformers/multi-qa-mpnet-base-dot-v1',
+                 do_strip=True, min_chapter_size=2000, first_chapter=0, last_chapter=math.inf,
+                 min_words_per_paragraph=150, max_words_per_paragraph=500):
         self._model = SentenceTransformer(model_name)
         self._file_name = full_file_name
         self._do_strip = do_strip
+        self._min_chapter_size = min_chapter_size
         self._first_chapter = first_chapter
         self._last_chapter = last_chapter
+        self._min_words = min_words_per_paragraph
+        self._max_words = max_words_per_paragraph
         self._chapters = None
         self._embeddings = None
 
-        if do_preview:
-            self.ebook_preview(self._do_strip)
-        else:
-            # Load the embeddings
-            if self.get_ext(self._file_name) == '.epub':
-                # Create a json file with the same name as the epub
-                json_file_name = self.switch_ext(self._file_name, '.json')
-                # Check if the json file exists
-                if not exists(json_file_name):
-                    # No json file exists, so create embeddings to put into a json file
-                    self.embed_epub(self._file_name, self._do_strip, self._first_chapter, self._last_chapter)
-                else:
-                    self._file_name = json_file_name
-            # A json file should now exist with our embeddings
-            assert self.get_ext(self._file_name) == '.json', 'Should now be a json file.'
-            # Do we now have embeddings and chapters? If not, load them
-            if self._chapters is None or self._embeddings is None:
-                self.load_embeddings_file()
+        if full_file_name is not None:
+            self.load_file(full_file_name)
 
-    @staticmethod
-    def format_paragraphs(chapters, min_words=150, max_words=500):
+    def load_model(self, model_name):
+        self._model = SentenceTransformer(model_name)
+
+    def load_file(self, full_file_name):
+        # Assert the full file name has an .epub or .json extension
+        assert self.get_ext(full_file_name) in ['.epub', '.json'], ('Invalid file format. '
+                                                                    'Please upload an epub or json file.')
+        self._file_name = full_file_name
+        # Load the embeddings
+        if self.get_ext(self._file_name) == '.epub':
+            # Create a json file with the same name as the epub
+            json_file_name = self.switch_ext(self._file_name, '.json')
+            # Check if the json file exists
+            if not exists(json_file_name):
+                # No json file exists, so create embeddings to put into a json file
+                self.embed_epub(self._file_name)
+            else:
+                self._file_name = json_file_name
+        # A json file should now exist with our embeddings
+        assert self.get_ext(self._file_name) == '.json', 'Should now be a json file.'
+        # Do we now have embeddings and chapters? If not, load them
+        if self._chapters is None or self._embeddings is None:
+            self.load_embeddings_file()
+
+    def format_paragraphs(self):
         # Split paragraphs that are too long and merge paragraphs that are too short
-        for i, chapter in enumerate(chapters):
+        for i, chapter in enumerate(self._chapters):
             for j, paragraph in enumerate(chapter['paragraphs']):
                 words = paragraph.split()
-                if len(words) > max_words:
+                if len(words) > self._max_words:
                     # Split the paragraph into two
                     # Insert paragraph with max words in place of the old paragraph
-                    maxed_paragraph = ' '.join(words[:max_words])
+                    maxed_paragraph = ' '.join(words[:self._max_words])
                     chapter['paragraphs'][j] = maxed_paragraph
                     # Insert a new paragraph with the remaining words
-                    new_paragraph = ' '.join(words[max_words:])
+                    new_paragraph = ' '.join(words[self._max_words:])
                     chapter['paragraphs'].insert(j + 1, new_paragraph)
 
                 # Merge paragraphs that are too short
-                while len(chapter['paragraphs'][j].split()) < min_words and j+1 < len(chapter['paragraphs']):
+                while len(chapter['paragraphs'][j].split()) < self._min_words and j+1 < len(chapter['paragraphs']):
                     # This paragraph is too short, so merge it with the next one
                     chapter['paragraphs'][j] += '\n' + chapter['paragraphs'][j+1]
                     # Delete the next paragraph since we just merged it to the previous one
@@ -67,7 +78,7 @@ class EBookSearch:
             last_index = len(chapter['paragraphs']) - 1
             last_para_len = len(chapter['paragraphs'][last_index].split())
             prev_para_len = len(chapter['paragraphs'][last_index - 1].split()) if last_index > 0 else 0
-            if last_para_len < min_words and last_index > 0 and prev_para_len + last_para_len < max_words:
+            if last_para_len < self._min_words and last_index > 0 and prev_para_len + last_para_len < self._max_words:
                 # Merge the last paragraph with the previous one
                 chapter['paragraphs'][last_index - 1] += '\n ' + chapter['paragraphs'][last_index]
                 # Remove the last paragraph since we just merged it to the previous one
@@ -129,7 +140,7 @@ class EBookSearch:
         # Convert to HTML and extract paragraphs
         html = BeautifulSoup(section.get_body_content(), 'html.parser')
         p_tag_list = html.find_all('p')
-        text_list = [para.get_text().strip() for para in p_tag_list if para.get_text().strip()]
+        text_list = [paragraph.get_text().strip() for paragraph in p_tag_list if paragraph.get_text().strip()]
         if len(text_list) == 0:
             return None
         # Extract and process headings
@@ -137,15 +148,15 @@ class EBookSearch:
         title = ' '.join(heading_list)
         return {'title': title, 'paragraphs': text_list}
 
-    @staticmethod
-    def strip_blank_chapters(chapters, first_chapter=0, last_chapter=math.inf, min_paragraph_size=2000):
+    def strip_blank_chapters(self):
         # This takes a list of _chapters and removes the ones outside the range [first_chapter, last_chapter]
-        last_chapter = min(last_chapter, len(chapters) - 1)
-        chapters = chapters[first_chapter:last_chapter + 1]
+        # or if the chapter is too small (likely a title page or something)
+        last_chapter = min(self._last_chapter, len(self._chapters) - 1)
+        chapters = self._chapters[self._first_chapter:last_chapter + 1]
         # Filter out _chapters with small total paragraph size
-        chapters = [chapter for chapter in chapters if sum(len(para) for para
-                                                           in chapter['paragraphs']) >= min_paragraph_size]
-        return chapters
+        chapters = [chapter for chapter in chapters if sum(len(paragraph) for paragraph
+                                                           in chapter['paragraphs']) >= self._min_chapter_size]
+        self._chapters = chapters
 
     def create_embeddings(self, texts):
         if type(texts) is str:
@@ -173,18 +184,18 @@ class EBookSearch:
             self.print_and_write(result_msg, f)
         self.print_and_write('\n', f)
 
-    def epub_to_chapters(self, epub_file_name, do_strip=True, first_chapter=0, last_chapter=math.inf):
+    def epub_to_chapters(self, epub_file_name):
         book = epub.read_epub(epub_file_name)
         item_doc = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
         book_sections = list(item_doc)
         chapters = [result for section in book_sections
                     if (result := self.epub_sections_to_chapter(section)) is not None]
-        if do_strip:
-            chapters = self.strip_blank_chapters(chapters, first_chapter, last_chapter)
-        self.format_paragraphs(chapters)
         self._chapters = chapters
+        if self._do_strip:
+            self.strip_blank_chapters()
+        self.format_paragraphs()
 
-    def embed_epub(self, epub_file_name, do_strip=True, first_chapter=0, last_chapter=math.inf):
+    def embed_epub(self, epub_file_name):
         # Assert this is an epub file
         assert self.get_ext(epub_file_name) == '.epub', 'Invalid file format. Please upload an epub file.'
         # Create a json file with the same name as the epub
@@ -195,7 +206,7 @@ class EBookSearch:
         # Create the json file name
         json_file_name = self.switch_ext(epub_file_name, '.json')
         # Convert the epub to html and extract the paragraphs
-        self.epub_to_chapters(epub_file_name, do_strip, first_chapter, last_chapter)
+        self.epub_to_chapters(epub_file_name)
         print('Generating _embeddings for "{}"\n'.format(epub_file_name))
         paragraphs = [paragraph for chapter in self._chapters for paragraph in chapter['paragraphs']]
         # Generate the _embeddings using the _model
@@ -213,14 +224,11 @@ class EBookSearch:
         except Exception as e:
             print(f'An unexpected error occurred: {e}')
 
-    def preview_epub(self, do_strip=True):
+    def preview_epub(self):
         epub_file_name = self._file_name
         assert self.get_ext(epub_file_name) == '.epub', 'Invalid file format. Please upload an epub file.'
-        self.epub_to_chapters(epub_file_name, do_strip)
+        self.epub_to_chapters(epub_file_name)
         self.print_previews(self._chapters)
-
-    def ebook_preview(self, do_strip=True):
-        self.preview_epub(do_strip)
 
     def load_embeddings_file(self):
         assert self.get_ext(self._file_name) == '.json', 'Should now be a json file.'
@@ -232,11 +240,14 @@ def test_ebook_search(do_preview=False):
     # noinspection SpellCheckingInspection
     book_path = \
         r'D:\Documents\Papers\EPub Books\Karl R. Popper - The Logic of Scientific Discovery-Routledge (2002).epub'
-    # book_path = r'D:\Documents\Papers\EPub Books\KJV.epub'
-    ebook_search = EBookSearch(book_path, do_preview=do_preview)
+    # book_path = r"D:\Documents\Papers\EPub Books\KJV.epub"
+    ebook_search = SemanticSearch()
     if not do_preview:
-        query = 'What makes a good explanation?'
+        ebook_search.load_file(book_path)
+        query = 'Why do we need to corroborate theories at all?'
         ebook_search.search(query, top_results=5)
+    else:
+        ebook_search.preview_epub()
 
 
 if __name__ == "__main__":
