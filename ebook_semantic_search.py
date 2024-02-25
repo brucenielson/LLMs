d7868ac3fdb6e83a52837bccc02c5880ee7178e6
Bruce Nielson <bnielson@mindfiretechnology.com>
2024-02-24 20:21:32 -0700

    Unit test is working

--- ebook_semantic_search.py
+++ ebook_semantic_search.py
@@ -217,6 +217,7 @@ class SemanticSearch:
         return self.read_json(path)
 
     def search(self, query, top_results=5):
+        results_msgs = []
         # Create _embeddings for the query
         query_embedding = self.create_embeddings(query)[0]
         scores = self.score(query_embedding, self._embeddings)
@@ -229,9 +230,10 @@ class SemanticSearch:
                 para, title, para_no = self.index_to_para_chapter_index(index, self._chapters)
                 result_msg = ('\nChapter: "{}", Passage number: {}, Score: {:.2f}\n"{}"'
                               .format(title, para_no, scores[index], para))
+                results_msgs.append(result_msg)
                 self.print_and_write(result_msg, f)
             self.print_and_write('\n', f)
-        return results
+        return results_msgs, results
 
     def epub_to_chapters(self, epub_file_name):
         book = epub.read_epub(epub_file_name)
@@ -308,6 +310,13 @@ class TestSemanticSearch(unittest.TestCase):
         self._json_path = \
             r'D:\Documents\Papers\EPub Books\Karl R. Popper - The Logic of Scientific Discovery-Routledge (2002).json'
 
+        # Check if the JSON file exists, if not, load EPUB and save embeddings in JSON
+        if not exists(self._json_path):
+            # Generate EPUB file path by replacing .json with .epub
+            epub_path = self._json_path.replace('.json', '.epub')
+            search_instance = SemanticSearch()
+            search_instance.embed_epub(epub_path)
+
     def test_query(self):
         # Test loading an EPUB file
         search_instance = SemanticSearch()
@@ -315,25 +324,31 @@ class TestSemanticSearch(unittest.TestCase):
 
         # Define your query and expected output
         query = 'Why do we need to corroborate theories at all?'
-        expected_sizes = [501, 441, 462, 465, 122]
-        # expected_results = [
-        #     {'chapter': '10 CORROBORATION, OR HOW A THEORY STANDS UP TO TESTS', 'passage_number': 60, 'score': 0.69},
-        #     {'chapter': '10 CORROBORATION, OR HOW A THEORY STANDS UP TO TESTS', 'passage_number': 0, 'score': 0.68},
-        #     {'chapter': '10 CORROBORATION, OR HOW A THEORY STANDS UP TO TESTS', 'passage_number': 21, 'score': 0.68},
-        #     {'chapter': '10 CORROBORATION, OR HOW A THEORY STANDS UP TO TESTS', 'passage_number': 24, 'score': 0.66}
-        # ]
-
+        expected_results = [501, 441, 462, 465, 122]
+        expected_results_msgs = '''
+            Chapter: "10 CORROBORATION, OR HOW A THEORY STANDS UP TO TESTS", Passage number: 60, Score: 0.69
+            "*6 See my Postscript, chapter *ii. In my theory of corroboration—in direct opposition to Keynes’s, 
+            Jeffreys’s, and Carnap’s theories of probability—corroboration does not decrease with testability, but 
+            tends to increase with it.
+            *7 This may also be expressed by the unacceptable rule: ‘Always choose the hypothesis which is most ad hoc!’
+            2 Keynes, op. cit., p. 305.
+            *8 Carnap, in his Logical Foundations of Probability, 1950, believes in the practical value of predictions; 
+            nevertheless, he draws part of the conclusion here mentioned—that we might be content with our basic 
+            statements. For he says that theories (he speaks of ‘laws’) are ‘not indispensable’ for science—not even 
+            for making predictions: we can manage throughout with singular statements. ‘Nevertheless’, he writes 
+            (p. 575) ‘it is expedient, of course, to state universal laws in books on physics, biology, psychology, 
+            etc.’ But the question is not one of expediency—it is one of scientific curiosity. Some scientists want 
+            to explain the world: their aim is to find satisfactory explanatory theories—well testable, i.e. simple 
+            theories—and to test them. (See also appendix *x and section *15 of my Postscript.)"
+            '''
         # Call the search method and get the actual results
-        actual_results = search_instance.search(query, top_results=5)
-        self.assertEqual(len(actual_results), len(expected_sizes))
-        # Check sizes of the passages
-        # for index, expected_size in zip(actual_results, expected_sizes):
-        #     para, _, _ = search_instance.index_to_para_chapter_index(index, search_instance._chapters)
-        #     actual_size = len(para.split())
-        #     self.assertEqual(actual_size, expected_size)
-
-        # Assert against the expected results
-        # self.assertEqual(actual_results, expected_results)
+        actual_results_msgs, actual_results = search_instance.search(query, top_results=5)
+        stripped_result_actual = (actual_results_msgs[0].replace(" ", "").replace("\t", "")
+                                  .replace("\n", ""))
+        stripped_expected = (expected_results_msgs.replace(" ", "").replace("\t", "")
+                             .replace("\n", ""))
+        self.assertEqual(expected_results, actual_results)
+        self.assertEqual(stripped_expected, stripped_result_actual)
 
     def tearDown(self):
         # Clean up any resources or configurations after testing
@@ -341,5 +356,6 @@ class TestSemanticSearch(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    test_ebook_search(do_preview=False)
+    # test_ebook_search(do_preview=False)
     # unittest.main()
+    pass
