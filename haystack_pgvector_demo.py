25ad3c04bce292696ad5bfdfa4bd0ad1f7977ae5
Bruce Nielson <bnielson@mindfiretechnology.com>
2024-07-12 19:48:59 -0600

    Got it to save off documents to PostgreSQL

--- haystack_pgvector_demo.py
+++ haystack_pgvector_demo.py
@@ -11,27 +11,28 @@ from ebooklib import epub, ITEM_DOCUMENT
 
 
 def load_epub(epub_file_path):
+    docs = []
     book = epub.read_epub(epub_file_path)
-    html_content = ""
 
     # Find all paragraphs across sections
     for section in book.get_items_of_type(ITEM_DOCUMENT):
         section_html = section.get_body_content().decode('utf-8')
         section_soup = BeautifulSoup(section_html, 'html.parser')
         paragraphs = section_soup.find_all('p')
-        html_content += ''.join([str(p) for p in paragraphs])  # Combine paragraph strings
+        byte_stream: ByteStream
+        for p in paragraphs:
+            p_str = str(p)
+            p_html = f"<html><head><title>Converted Epub</title></head><body>{p_str}</body></html>"
+            byte_stream = ByteStream(p_html.encode('utf-8'))
+            docs.append(byte_stream)
 
+    # html_content += ''.join([str(p) for p in paragraphs])  # Combine paragraph strings
     # Add missing elements (optional)
-    html_content = f"<html><head><title>Converted Epub</title></head><body>{html_content}</body></html>"
-
-    html_bytes = html_content.encode('utf-8')
-    html_byte_stream = ByteStream(html_bytes)
-
-    return html_byte_stream
+    return docs
 
 
 # Initialize and load documents
-def initialize_and_load_documents(epub_file_path, recreate_table=True):
+def initialize_and_load_documents(epub_file_path, recreate_table=False):
     document_store = PgvectorDocumentStore(
         table_name="federalist_papers",
         embedding_dimension=768,
@@ -43,38 +44,37 @@ def initialize_and_load_documents(epub_file_path, recreate_table=True):
 
     if document_store.count_documents() == 0:
         # Convert EPUB to text documents
-        document = load_epub(epub_file_path)
-
-        # Convert bytes to a list (required by the converter)
-        sources = [document]
+        sources = load_epub(epub_file_path)
 
         converter = HTMLToDocument()
         results = converter.run(sources=sources)
+        converted_docs = results["documents"]
+        converted_docs = [Document(content=doc.content) for doc in converted_docs if doc.content is not None]
+
+        # Remove duplicate Documents with duplicates with duplicate document ids
+        converted_docs = list({doc.id: doc for doc in converted_docs}.values())
 
         # Clean the documents
         cleaner = DocumentCleaner()
-        cleaned_docs = cleaner.run(documents=results["documents"])
+        cleaned_docs = cleaner.run(documents=converted_docs)
 
         # Split the documents
-        splitter = DocumentSplitter(
-            split_by="word",
-            split_length=200,
-            split_overlap=20,
-        )
+        splitter = DocumentSplitter(split_by="word",
+                                    split_length=500,
+                                    split_overlap=0,
+                                    split_threshold=50)
         split_docs = splitter.run(documents=cleaned_docs["documents"])
-
-        document_store.write_documents(split_docs["documents"])
+        docs_with_embeddings = create_embeddings(split_docs['documents'])
+        document_store.write_documents(docs_with_embeddings['documents'])
 
     return document_store
 
 
-# Set up indexing pipeline
-def setup_indexing_pipeline():
-    embedder = SentenceTransformersDocumentEmbedder(model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1")
-
-    indexing_pipeline = Pipeline()
-    indexing_pipeline.add_component("embedder", embedder)
-    return indexing_pipeline
+def create_embeddings(documents):
+    document_embedder = SentenceTransformersDocumentEmbedder()
+    document_embedder.warm_up()
+    documents_with_embeddings = document_embedder.run(documents)
+    return documents_with_embeddings
 
 
 # Run indexing pipeline
@@ -102,11 +102,9 @@ def setup_query_pipeline(document_store):
 def main():
     epub_file_path = "Federalist Papers.epub"
     document_store = initialize_and_load_documents(epub_file_path)
-
-    indexing_pipeline = setup_indexing_pipeline()
-    run_indexing_pipeline(indexing_pipeline, document_store)
-
     query_pipeline = setup_query_pipeline(document_store)
+    result = query_pipeline.run(query="What is the role of the judiciary in a democracy?")
+    print(result)
 
 
 main()
