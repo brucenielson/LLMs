1e6249d92f947b72c2f2783b2308939b4728580d
Bruce Nielson <bnielson@mindfiretechnology.com>
2024-07-14 22:25:30 -0600

    Gemma demo working

--- gemma_demo.py
+++ gemma_demo.py
@@ -7,7 +7,6 @@ secret_file = r'D:\Documents\Secrets\huggingface_secret.txt'
 try:
     with open(secret_file, 'r') as file:
         secret_text = file.read()
-        print(secret_text)
 except FileNotFoundError:
     print(f"The file '{secret_file}' does not exist.")
 except Exception as e:
@@ -15,9 +14,9 @@ except Exception as e:
 
 login(secret_text)
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-9b-it")  # google/gemma-1.1-2b-it
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-1.1-2b-it")  # google/gemma-2-9b-it
 model = AutoModelForCausalLM.from_pretrained(
-    "google/gemma-2-9b-it",
+    "google/gemma-1.1-2b-it",
     device_map="auto",
     torch_dtype=torch.bfloat16
 )
@@ -25,5 +24,5 @@ model = AutoModelForCausalLM.from_pretrained(
 input_text = "Write me a poem about Machine Learning."
 input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
-outputs = model.generate(**input_ids)
+outputs = model.generate(**input_ids, max_length=2000).to("cuda")
 print(tokenizer.decode(outputs[0]))
